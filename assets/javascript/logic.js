e7905635325cd219fea32248989418ee4794570b
andyymontenegro <ajrunner10@gmail.com>
2017-09-06 13:43:05 -0500

    started java firebase/clock

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,21 @@
-alert("it works");
+//alert("it works");
+$(document).ready(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB2BJc8gRryhTrZ7LpeMC02ptCSeiRdxTI",
+    authDomain: "train-scheduler-5e5c8.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5e5c8.firebaseio.com",
+    projectId: "train-scheduler-5e5c8",
+    storageBucket: "train-scheduler-5e5c8.appspot.com",
+    messagingSenderId: "660863966122"
+  };
+  firebase.initializeApp(config);
+
+  //clock
+  setInterval(function(){
+      $('.current-time').html(moment().format("hh:mm:ss A"))
+  }, 1000);
+
+  $(".content").hide();
+});
